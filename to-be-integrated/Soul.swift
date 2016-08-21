2bf0afea195ece56b6b6e058e7b05105b12dba92
June <kimjune01@gmail.com>
2016-08-20 17:10:09 -0700

    added tested yet unintegrated WIP without project

---
+++ to-be-integrated/Soul.swift
@@ -0,0 +1,70 @@
+
+
+import UIKit
+import TheAmazingAudioEngine
+
+
+
+class Soul: NSObject {
+  var s3Key:String?
+  var localURL:String?
+  var epoch:Int?
+  var longitude:Double?
+  var latitude:Double?
+  var radius: Double?
+  var token: String?
+//  var device: Device?
+  var type: SoulType?
+  
+  enum SoulType: String {
+    case Incoming = "Incoming"
+    case Outgoing = "Outgoing"
+  }
+  
+  // Usage: postToServer(someSoul.toParams())
+  func toParams(type:SoulType) -> NSDictionary {
+    let contentParams = NSMutableDictionary()
+    contentParams["type"] = type.rawValue
+    contentParams["s3Key"] = s3Key
+    contentParams["epoch"] = epoch
+    contentParams["longitude"] = longitude
+    contentParams["latitude"] = latitude
+    contentParams["radius"] = radius
+    contentParams["token"] = token
+    return contentParams
+  }
+  
+  
+  class func from(incomingParams:NSDictionary) -> Soul {
+    let contentSoul = Soul()
+    contentSoul.type = SoulType(rawValue: incomingParams["type"] as! String)
+    contentSoul.s3Key = incomingParams["s3Key"] as? String
+    contentSoul.epoch = incomingParams["epoch"] as? Int
+    contentSoul.latitude = (incomingParams["latitude"] as! NSString).doubleValue
+    contentSoul.longitude = (incomingParams["longitude"] as! NSString).doubleValue
+    contentSoul.radius = (incomingParams["radius"] as! NSString).doubleValue
+    return contentSoul
+  }
+  
+  func printProperties() {
+    print("s3Key: \(s3Key)")
+    print("localURL: \(localURL)")
+    //TODO:
+  }
+  
+}
+
+class MockFactory {
+  static func mockSoulOne() -> Soul {
+    let newMockSoul = Soul()
+    newMockSoul.s3Key = "helloEricTesting"
+    newMockSoul.localURL = "/Users/june/Library/Developer/CoreSimulator/Devices/773EB184-EEE2-499F-AB97-F63AEF6FC7FB/data/Containers/Data/Application/A102AB98-3348-4B4B-A4F4-97E2714EB03B/Documents/Recording493417238.887139.m4a"
+    newMockSoul.epoch = 493417238
+    newMockSoul.longitude = 49.343
+    newMockSoul.latitude = 223.234
+    newMockSoul.radius = 44.44
+    newMockSoul.token = "SOMESAMPLETOKENBLAHBLAH"
+    
+    return newMockSoul
+  }
+}
---
+++ to-be-integrated/SwiftJK.swift
@@ -0,0 +1,36 @@
+import Foundation
+import UIKit
+import Alamofire
+
+class SwiftJK {
+  
+}
+
+func doOnce (taskName:String, task:() -> ()) -> (Bool) {
+  if NSUserDefaults.standardUserDefaults().valueForKey("doOnce-" + taskName) == nil {
+    task()
+    NSUserDefaults.standardUserDefaults().setValue(true, forKey: "doOnce-" + taskName)
+    return true
+  } else {
+    return false
+  }
+}
+
+var debugging = true
+
+func printline(string:String) {
+  if debugging {
+    print(string)
+  }
+}
+
+extension UILabel {
+  func decorateWhite(fontSize:CGFloat) {
+    self.textColor = UIColor.whiteColor()
+    self.shadowColor = UIColor.blackColor().colorWithAlphaComponent(0.3)
+    self.shadowOffset = CGSize(width: 1, height: 1)
+    self.font = UIFont(name: "Helvetica", size: fontSize)
+    self.textAlignment = NSTextAlignment.Center
+    
+  }
+}
