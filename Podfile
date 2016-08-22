719053ff0430c70577c6740f004a522c682c9ff5
June <kimjune01@gmail.com>
2016-08-22 13:15:02 -0700

    added blank project with dependencies

--- Podfile
+++ Podfile
@@ -1,11 +1,10 @@
 # Uncomment this line to define a global platform for your project
 platform :ios, '9.0'
 
-target 'BoxedSoul' do
+target 'Soulcast' do
   # Comment this line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
-  # Pods for BoxedSoul
 	pod "TheAmazingAudioEngine"
 pod 'Alamofire', '~> 3.4'    
 pod 'AWSCognito'
---
+++ Soulcast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Soulcast
+//
+//  Created by June Kim on 2016-08-22.
+//  Copyright © 2016 Soulcast-team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
