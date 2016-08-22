c9e00030f1e2f459d2c087ba301839b99a00f4db
June <kimjune01@gmail.com>
2016-08-22 14:34:47 -0700

    added custom view controller and junetoken

---
+++ Soulcast/MainVC.swift
@@ -0,0 +1,22 @@
+//
+//  MainVC.swift
+//  Soulcast
+//
+//  Created by June Kim on 2016-08-22.
+//  Copyright © 2016 Soulcast-team. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainVC: UIViewController {
+  //
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = UIColor.blueColor()
+  }
+  
+  
+  
+}
---
+++ Soulcast/ModelTests.swift
@@ -0,0 +1,17 @@
+//
+//  ModelTests.swift
+//  Soulcast
+//
+//  Created by June Kim on 2016-08-22.
+//  Copyright © 2016 Soulcast-team. All rights reserved.
+//
+
+import Foundation
+
+let modelTester = ModelTest()
+
+class ModelTest {
+  func testAllTheThings() {
+    print("Test All The Things here!")
+  }
+}
