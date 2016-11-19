2cabe9ce0dfe90b0686781db5bed164c78fde0d0
June <kimjune01@gmail.com>
2016-11-19 12:41:52 -0800

    index on june-dev: b803c44 added improve feature

---
+++ Soulcast/HistoryVC.swift
@@ -0,0 +1,9 @@
+//
+//  HistoryVC.swift
+//  Soulcast
+//
+//  Created by June Kim on 2016-11-17.
+//  Copyright © 2016 Soulcast-team. All rights reserved.
+//
+
+import Foundation
---
+++ Soulcast/MainCoordinator.swift
@@ -0,0 +1,9 @@
+//
+//  MainCoordinator.swift
+//  Soulcast
+//
+//  Created by June Kim on 2016-11-17.
+//  Copyright © 2016 Soulcast-team. All rights reserved.
+//
+
+import Foundation
